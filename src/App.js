dc5eeb8ed2e4b8b42bdc4b3cb9885edcd32c86b5
Valerie Sidman <valerie.sidman@gmail.com>
2021-03-19 12:54:51 +0300

    added function withLabels

--- src/App.js
+++ src/App.js
@@ -1,5 +1,81 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React, { useState } from 'react';
-import List from '../src/components/List'
+
+function New(props) {
+  return (
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
+    </div>
+  )
+};
+
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  )
+};
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3><a href="/#">{props.title}</a></h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  )
+};
+
+function Video(props) {
+  return (
+    <div className="item item-video">
+      <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  )
+};
+
+function List(props) {
+  return props.list.map(item => {
+    switch (item.type) {
+      case 'video':
+        return (
+          withLabels(Video)(item)
+        );
+
+      case 'article':
+        return (
+          withLabels(Article)(item)
+        );
+
+      default:
+        return null;
+    }
+  });
+};
+
+function withLabels(Component) {
+  function UpgradedComponent(props) {
+    console.log(props);
+    if (props.views < 100) return (
+      <New {...props}>
+        <Component {...props} />
+      </New>
+    )
+
+    if (props.views >= 1000) return (
+      <Popular {...props}>
+        <Component {...props} />
+      </Popular>
+    )
+
+    return <Component {...props} />
+  }
+
+  return UpgradedComponent;
+}
 
 export default function App() {
   const [list] = useState([
